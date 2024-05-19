558a549db3522b2bd96429e8ea790098cf7cfa30
Junaid Saleem <junaidsaleem986@gmail.com>
2024-05-19 18:06:45 +0500

    Create input.cpp

---
+++ input.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// Recursive factorial function
+unsigned long long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+short int main() {
+    int num;
+    cout << "Enter a positive integer: ";
+    cin >> num;
+
+    if (num < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+    } else {
+        unsigned long long result = factorial(num);
+        cout << "Factorial of " << num << " is: " << result << endl;
+    }
+
+    return 0;
+}
